1a049867d210be449ee7aaf0ba0332d876272a54
James Halliday <mail@substack.net>
2014-08-29 06:02:02 -0700

    passing nobreak test

---
+++ test/nobreak.js
@@ -0,0 +1,19 @@
+var parse = require('../');
+var test = require('tape');
+var fs = require('fs');
+
+test('no break', function (t) {
+    t.plan(1);
+    var w = parse();
+    w.on('headers', function (headers) {
+        t.deepEqual(headers, {
+            foo: '111',
+            bar: '222',
+            baz: '333'
+        });
+    });
+    w.write('foo: 111\r\n');
+    w.write('bar: 222\r\n');
+    w.write('baz: 333\r\n');
+    w.end();
+});
